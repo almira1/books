bb5aad54e1a9e9dac56ab34a8fa1f1d5709af614
edzino1 <elma.dzino@gmail.com>
2014-12-30 21:44:40 +0100

    confirmation dialog on delete

--- src/main/webapp/scripts/directives.js
+++ src/main/webapp/scripts/directives.js
@@ -125,6 +125,22 @@ angular.module('booksApp')
             }
         };
     })
+    
+    .directive('ngReallyClick', [function() {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            element.bind('click', function() {
+                var message = attrs.ngReallyMessage;
+                if (message && confirm(message)) {
+                    scope.$apply(attrs.ngReallyClick);
+                }
+            });
+        }
+    }
+}])
+    
+    
     .directive('starRating',
 		  function() {
 		    return {
